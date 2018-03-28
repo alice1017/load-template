7931e467a77f7de191a724277ae8c38238ce32cc
Alice1017 <genocidedragon@gmail.com>
2018-03-28 06:50:10 -0400

    feat: implement the process to create file

--- load_template/__main__.py
+++ load_template/__main__.py
@@ -5,6 +5,11 @@ import sys
 # import traceback
 
 from .cli import parser
+from .core import (
+    TemplateLoader,
+    render
+)
+from .util import parse_variables
 
 
 def main(argv=sys.argv):
@@ -14,8 +19,21 @@ def main(argv=sys.argv):
         sys.exit(0)
 
     args = parser.parse_args()
-
     print args
+
+    loader = TemplateLoader()
+    template_contents = loader.load_template(args.template)
+
+    if len(args.vars) != 0:
+        variable_data = parse_variables(args.vars)
+        contents = render(template_contents, variable_data)
+
+    else:
+        contents = template_contents
+
+    print contents
+    with open(args.filename, "w") as fp:
+        fp.write(contents)
 
 
 if __name__ == '__main__':
