59ed40f6baa7e3fc040cf248d6239ba8e507204a
Alice1017 <genocidedragon@gmail.com>
2018-03-27 23:01:24 -0400

    feat: implement argument parser

---
+++ load_template/cli.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from argparse import (
+    ArgumentParser,
+    OPTIONAL,
+    ZERO_OR_MORE,
+    RawDescriptionHelpFormatter
+)
+
+parser = ArgumentParser(
+    prog="load-template",
+    formatter_class=RawDescriptionHelpFormatter,
+    description="Create a file from the template with the variables.\n"
+                "You can create the templates as freely "
+                "if you save it '~/.templates/' directory."
+)
+
+parser.add_argument(
+    "template",
+    action="store",
+    nargs=OPTIONAL,
+    default=None,
+    help="The template name. You can show all templates by '--list'")
+
+parser.add_argument(
+    "filename",
+    metavar="file",
+    action="store",
+    nargs=OPTIONAL,
+    default=None,
+    help="The file name to create.")
+
+parser.add_argument(
+    "vars",
+    metavar="variables",
+    action="store",
+    nargs=ZERO_OR_MORE,
+    default=None,
+    help="The variables formatted 'key=value'.")
+
+parser.add_argument(
+    "-l", "--list",
+    action="store_true",
+    help="Display the template list.")
+
+parser.add_argument(
+    "-D", "--dev",
+    action="store_true",
+    help="Run development mode.")
