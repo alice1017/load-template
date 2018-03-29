3f4ebde9636b083b708b08c92f754d1bab1aa38b
Alice1017 <genocidedragon@gmail.com>
2018-03-29 01:12:51 -0400

    write: write display function test

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import sys
+
 from unittest import TestCase
 from load_template.util import (
     parse_variables,
-    separate
+    separate,
+    display_list
 )
 
 
@@ -36,3 +39,35 @@ class UtilitiesTestCase(TestCase):
             list(separate(testcase, 2)),
             ["12", "34", "56", "78", "9"]
         )
+
+    def test_display(self):
+
+        testcase = ["Alice", "Bob", "Python"]
+        result = "* Alice " + " " * 5 + "\n"
+        result += "* Bob   " + " " * 5 + "\n"
+        result += "* Python" + " " * 5 + "\n"
+        fp = self._setup_testcase()
+
+        display_list(testcase)
+
+        stdout = self._get_stdout(fp)
+
+        self.assertEqual(stdout, result)
+
+    def _setup_testcase(self):
+
+        tmpfile = "/tmp/load-template-tmpfile"
+        fp = open(tmpfile, "w")
+
+        sys.stdout = fp
+
+        return fp
+
+    def _get_stdout(self, fp):
+
+        sys.stdout = sys.__stdout__
+        fp.close()
+
+        tmpfile = "/tmp/load-template-tmpfile"
+        with open(tmpfile, "r") as fp:
+            return fp.read()
